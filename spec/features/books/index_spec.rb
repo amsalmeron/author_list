c4479c8282ea0caf03c9ee36d28b2cfb0288f608
Antonio Salmeron <mr.tonyboi@gmail.com>
2022-05-16 12:00:41 -0700

    Add: link to update book attributes from books index page

--- spec/features/books/index_spec.rb
+++ spec/features/books/index_spec.rb
@@ -33,5 +33,27 @@ RSpec.describe 'Book Index Page' do
         click_link "All Authors"
         expect(current_path).to eq("/authors")
     end
+
+    it "has link to edit book attributes" do
+        author_1 = Author.create!(name: 'Antonio', age: 1, alive: false)
+        author_2 = Author.create!(name: 'Larry', age: 2, alive: true)
+
+        book_1 = author_1.books.create!(title: 'Lala Land', page_count: 100, fiction: true)
+        book_2 = author_1.books.create!(title: 'Volcano Island', page_count: 200, fiction: false)
+
+        book_3 = author_2.books.create!(title: 'Voyager', page_count: 300, fiction: false)
+        book_4 = author_2.books.create!(title: 'Miss Patty', page_count: 400, fiction: true)
+        
+        visit '/books'
+
+        click_link "Update #{book_1.title}"
+        expect(current_path).to eq("/books/#{book_1.id}/edit")
+        fill_in "Title",	with: "Jungle Book"
+        fill_in "Page count",	with: "400"
+        click_button "Submit"
+        expect(current_path).to eq("/books/#{book_1.id}")
+        expect(page).to have_content("Jungle Book") 
+        expect(page).to have_content("400") 
+    end
     
 end
